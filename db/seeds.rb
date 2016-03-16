c87e003bd70b9141e30518872718356caa3b8f6a
tadeosss <stadeo17@gmail.com>
2016-03-16 23:47:09 +0000

    Assignment 30 completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ posts = Post.all
     )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A unique title", body: "A unique body")
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Comment.find_or_create_by(body: "A unique body")
+puts "#{Comment.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
