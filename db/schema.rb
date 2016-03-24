84bd6b4ce18a91ff95cea102a76c37c93cbd2f81
tadeosss <stadeo17@gmail.com>
2016-03-24 13:17:24 +0000

    display questions in index

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160323025613) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
