ab31fed02f04100aef476e881bee941aebf530ce
Xander Miller <xander.miller@gmail.com>
2016-03-30 20:52:36 -0400

    cleans up specs to use shoulda properly

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  it { is_expected.to belong_to(:topic) }
+  describe "attributes" do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it { should have_db_column(:title).of_type(:string) }
+    it { should have_db_column(:body).of_type(:text) }
+  end
   
-  describe "attributes" do
-      
-      it "responds to title" do
-        expect(post).to respond_to(:title)
-      end
-      
-      it "responds to body" do
-        expect(post).to respond_to(:body)
-      end
-    end
+  describe 'associations' do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it { should have_many(:comments) }
+    it { should belong_to(:topic) }
+  end
+
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,25 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-  it { is_expected.to have_many(:posts) }
+  describe "attributes" do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:description).of_type(:text) }
+    it { should have_db_column(:public).of_type(:boolean).with_options(default: true) }
+  end
 
-  describe "attributes" do
-    it "responds to name" do
-      expect(topic).to respond_to(:name)
-    end
-
-    it "responds to description" do
-      expect(topic).to respond_to(:description)
-    end
-
-    it "responds to public" do
-      expect(topic).to respond_to(:public)
-    end
-
-    it "is public by default" do
-      expect(topic.public).to be(true)
-    end
+  describe 'associations' do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it { should have_many(:posts) }
   end
 end
