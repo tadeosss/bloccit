ca33455b2ebf2856cb455e751ca57bc546456d2c
Xander Miller <xander.miller@gmail.com>
2016-04-08 14:27:33 -0400

    replaces examples with shoulda style examples

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  #around_save { self.email = email.downcase }
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
